f8724f1a406d41dd81d12aa7d66f098bb8a7ae4f
Nallely16 <69940806+Nallely16@users.noreply.github.com>
2025-06-05 02:52:36 -0500

    Update userRepository.js

--- server/userRepository.js
+++ server/userRepository.js
@@ -1,12 +1,10 @@
 // server/userRepository.js
 
 async function findUserByEmail(email) {
-  // Esta función será mockeada en los tests
   return null;
 }
 
 async function saveUser(userData) {
-  // También será mockeada
   return {
     id: 1,
     ...userData,
